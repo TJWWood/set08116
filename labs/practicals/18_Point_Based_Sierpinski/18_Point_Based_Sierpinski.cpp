48cc94dc0bff2f191e3f414e0951057d2b70f07d
TJWWood <40295704@live.napier.ac.uk>
2018-01-15 15:55:47 +0000

    working on 18

--- labs/practicals/18_Point_Based_Sierpinski/18_Point_Based_Sierpinski.cpp
+++ labs/practicals/18_Point_Based_Sierpinski/18_Point_Based_Sierpinski.cpp
@@ -28,15 +28,15 @@ void create_sierpinski(geometry &geom) {
   for (auto i = 1; i < num_points; ++i) {
     // *********************************
     // Add random point
-
+	  points.push_back(//GASKET ALGORITHM GOES HERE
     // Add colour - all points red
-
+	  colours.push_back(vec4(1.0f, 0.0f, 0.0f, 1.0f));
     // *********************************
   }
   // *********************************
   // Add buffers to geometry
-
-
+  geom.add_buffer(points, BUFFER_INDEXES::POSITION_BUFFER);
+  geom.add_buffer(colours, BUFFER_INDEXES::COLOUR_BUFFER);
   // *********************************
 }
 
